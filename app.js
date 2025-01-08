a2833b200c3cf2ea3a466e94c9ad6c7df57f8d6b
mirziyo <mirziyo>
2025-01-08 10:43:30 +0500

    Refactor age calculation script and add name validation feature

--- app.js
+++ app.js
@@ -2,18 +2,28 @@
 
 // 1
 
-const ism = prompt ('Isminggizni krting')
-const yosh = prompt ('Yoshingizni krting')
+const ism = prompt("Isminggizni krting");
+const yosh = prompt("Yoshingizni krting");
 
-let tuglganYil = 2025 - yosh
-let oy = yosh * 12
-let hafta = ((yosh * 365) / 7).toFixed(0)
-let kun = yosh * 365
-let soat = kun * 24
-let daqiqa = soat * 60 
-let soniya = daqiqa * 60 
+let tuglganYil = 2025 - yosh;
+let oy = yosh * 12;
+let hafta = ((yosh * 365) / 7).toFixed(0);
+let kun = yosh * 365;
+let soat = kun * 24;
+let daqiqa = soat * 60;
+let soniya = daqiqa * 60;
 
-const result = `Qadrli ${ism}. Siz ${tuglganYil} yilda tug'lgansiz va siz ${oy} oy, ${hafta} hafta, ${kun} kun, ${soat} soat, ${daqiqa} daqiqa, ${soniya} soniya yashagansiz.`
+const result = `Qadrli ${ism}. Siz ${tuglganYil} yilda tug'lgansiz va siz ${oy} oy, ${hafta} hafta, ${kun} kun, ${soat} soat, ${daqiqa} daqiqa, ${soniya} soniya yashagansiz.`;
 
-alert (result)
+alert(result);
 
+// 2
+
+const ims = prompt("Ismni krting");
+
+const ismlar = ["Alisher", "Nurmuhammad", "Hayrulla"];
+if (ismlar.includes(ism)) {
+  alert(`Ha ${ism}mi bor.😊`);
+} else {
+  alert(`Afsuski ${ism}mi topilmadi.😔`);
+}
